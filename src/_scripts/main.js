da6556e779dcd84a11145aa1a673f055004efbf5
Mike <migueljrojas@gmail.com>
2019-05-18 20:36:02 -0300

    bootstrap 4

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -9,7 +9,6 @@ var Header = require('../_modules/header/header');
 var Slider = require('../_modules/slider/slider');
 
 $(function() {
-    require('../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min');
     require('../../bower_components/slick-carousel/slick/slick');
 
     new Header();
